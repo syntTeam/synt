7413b8c34b653c77b5b023a1c81603b3d7642d4a
eclipsedevbcfunny <eclipsedevbcfunny@github.com>
2024-03-19 20:30:31 -0400

    jaugh

---
+++ Makefile
@@ -0,0 +1,31 @@
+# Define compiler and flags
+NASM=nasm
+NASMFLAGS=-f elf32
+GCC=gcc
+GCCFLAGS=-m32
+LD=ld
+LDFLAGS=-m elf_i386
+QEMU=qemu-system-i386
+
+# Define source files
+SRCDIR=src
+BOOT_ASM=$(SRCDIR)/boot.asm
+KERNEL_C=$(SRCDIR)/kernel.c
+LINKER_LD=$(SRCDIR)/linker.ld
+
+# Default target
+all: kernel
+
+# Linking the kernel
+kernel: boot.o kernel.o
+	$(LD) $(LDFLAGS) -T $(LINKER_LD) -o kernel boot.o kernel.o
+	rm -f boot.o kernel.o
+	$(QEMU) -kernel kernel
+
+# Assembling boot.asm
+boot.o: $(BOOT_ASM)
+	$(NASM) $(NASMFLAGS) $< -o $@
+
+# Compiling kernel.c
+kernel.o: $(KERNEL_C)
+	$(GCC) $(GCCFLAGS) -c $< -o $@
---
+++ src/kernel.c
@@ -0,0 +1,49 @@
+#define WHITE_TXT 0x2F
+
+void k_clear_screen();
+unsigned int k_printf(char *message, unsigned int line);
+
+void k_main() 
+{
+	k_clear_screen();
+	k_printf("Synt, the one for you.", 0);
+};
+
+void k_clear_screen()
+{
+	char *vidmem = (char *) 0xb8000;
+	unsigned int i=0;
+	while(i < (80*25*2))
+	{
+		vidmem[i]=' ';
+		i++;
+		vidmem[i]=WHITE_TXT;
+		i++;
+	};
+};
+
+unsigned int k_printf(char *message, unsigned int line)
+{
+	char *vidmem = (char *) 0xb8000;
+	unsigned int i=0;
+
+	i=(line*80*2);
+
+	while(*message!=0)
+	{
+		if(*message=='\n')
+		{
+			line++;
+			i=(line*80*2);
+			*message++;
+		} else {
+			vidmem[i]=*message;
+			*message++;
+			i++;
+			vidmem[i]=WHITE_TXT;
+			i++;
+		};
+	};
+
+	return(1);
+}
